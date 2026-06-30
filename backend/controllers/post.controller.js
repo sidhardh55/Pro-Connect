a08e528ee93b65bcd2048b4b26651b4f125727cf
sidhu <sidhumanchukonda@gmail.com>
2026-06-30 20:34:56 +0530

    completed backend

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,7 +1,186 @@
+import User from "../models/user.model.js";
+import Post from "../models/posts.model.js";
 
-export const activeCheck = async (req,res) => {
+export const activeCheck = async (req, res) => {
+    return res.status(200).json({ message: "RUNNING" });
+};
 
-    return res.status(200).json({message: "RUNNING" })
+export const createPost = async (req, res) => {
 
+    const token = req.body?.token || req.headers?.authorization || "";
+    const bodyText = req.body?.body || req.body?.text || req.body?.message || req.body?.content || "";
+
+    try {
+        if (!token) {
+            return res.status(400).json({ message: "Token is required" });
+        }
+
+        const user = await User.findOne({ token });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const post = new Post({
+            userId: user._id,
+            body: bodyText || "test-post",
+            media: req.file ? req.file.filename : "",
+            fileType: req.file ? req.file.mimetype.split("/")[1] : ""
+        });
+
+        await post.save(); 
+
+        return res.status(200).json({ message: "Post created" });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+};
+
+export const getAllPosts = async (req,res) =>{
+    try {
+
+        const posts =await Post.find().populate('userId','name username email profilePicture')
+        return res.json({ posts })
+
+    }catch(err){
+        return res.status(500).json({messege:err.messege})
+    }
 }
 
+
+export const deletePost = async (req,res) =>{
+
+    const {token,post_id} = req.body;
+
+    try{
+        const user = await User.findOne({token}).select("_id");
+
+        if(!user){
+            return res.status(404).json({messege:"User not found"});
+        }
+
+        const post = await Post.find({_id:post_id});
+
+        if(!post){
+            return res.status(404).json({messege:"Post not found"});
+        }
+
+        if(post.userId.toString() !== user._id.toString()){
+            return res.status(401).json({messege : "Unauthorized"})
+        }
+
+        await Pot.deletePost({_id:post_id});
+
+        return res.json({messege:"Post Deleted"})
+
+
+    }catch(err){
+        res.status(500).json({message:err.message});
+    }
+}
+
+export const commentPost = async (req,res) =>{
+    
+    const {token,post_id,commentBody} = req.body;
+
+    try{
+        const user = await User.findOne({token}).select("_id");
+
+        if(!user){
+            return res.status(404).json({messege: "User not found "});
+        }
+
+        const post =await Post.findOne({
+            _id:post_id
+        })
+
+        if(!post){
+            return res.status(404).json({messege: "Post not found "});
+        }
+
+        const comment = new Comment({
+            userId : user._id,
+            postId:post_id,
+            comment:commentBody
+        });
+
+        await comment.save();
+        
+        return res.status(200).json({messege:"Comment Added"});
+
+    }catch(err){
+        res.status(500).json({messege:err.messege});
+    }
+}
+
+
+export const get_comments_by_post = async(req,res) =>{
+    const {post_id} = req.body;
+
+    try{
+        const post = await Post.findOne({_id:post_id});
+
+        if(!post){
+            return res.status(404).json({messege: "Post not found "});
+        }
+
+        return res.json({comments:post.comments})
+
+    }catch(err){
+        
+        return res.status(500).json({messege:err.messeg});
+
+    }
+}
+
+export const delete_comment_of_user = async(req,res)=>{
+    const {token,comment_id} = req.body;
+
+    try{
+        const user = await User.findOne({token}).select("_id");
+
+        if(!user){
+            return res.status(404).json({messege: "User not found "});
+        }
+
+        const comment =  await Comment.findOne({_id:comment_id})
+
+        if(!comment){
+            res.status(404).json({messeg :"Comment not found "});
+        }
+
+        if(comment.userId.toString() !== user._id.toString()){
+            return res.status(401).json({messege:"Unauthorized"});
+        }
+    
+        await Comment.deleteOne({"_id":comment_id});
+
+        return res.json({messeg:"Comment Deleted"});
+
+    }catch(err){
+        return res.status(500).json({messege:err.messege});
+    }
+}
+
+export const increment_likes = async(req,res)=>{
+
+    const {post_id} = req.body;
+
+    try{
+        
+        const post =await Post.findOne({_id:post_id});
+       
+        if(!post){
+            return res.status(404).json({messege: "Post not found "});
+        }
+
+        post.likes = post.likes + 1 ;        
+
+        await post.save();
+
+        return res.json({messege:"Likes Incremented"});
+
+    }catch(err){
+        res.status(500).json({messege:err.messege});
+    }
+}
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,12 +1,29 @@
 import { Router } from "express";
-import { activeCheck } from "../controllers/post.controller.js";
+import { activeCheck,createPost, getAllPosts,deletePost, delete_comment_of_user,commentPost,get_comments_by_post,increment_likes} from "../controllers/post.controller.js";
+import multer from "multer";
+
 
 const router =Router();
 
+const storage = multer.diskStorage({
+    destination : (req,file,cb) =>{
+        cb(null,'uploads/')
+    },
+    filename:(req,file,cb) =>{
+        cb(null,file.originalname)
+    },
+})
 
+const upload  = multer({storage:storage})
 
 router.route('/').get(activeCheck);
 
-
+router.route("/post").post(upload.single('media'),createPost)
+router.route("/posts").get(getAllPosts);
+router.route("/delete_post").post(deletePost);
+router.route("/comment").post(commentPost);
+router.route("/get_comments").get(get_comments_by_post);
+router.route("/delete_comment").delete(delete_comment_of_user);
+router.route("/increment_post_like").post(increment_likes);
 
 export default router;
